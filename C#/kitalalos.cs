7acb8ae7a73b9d8081b1bf3730b39cb9498f96dd
takozs <43068074+takozs@users.noreply.github.com>
2018-10-02 10:03:53 +0200

    Add files via upload

---
+++ C#/kitalalos.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r= new Random();
+            int szam = r.Next(1,1001);
+            int szam1 = 0;
+            int szam2 = 0;
+            int szam3 = 0;
+            int szam4 = 0;
+            int szam5 = 0;
+
+
+            Console.WriteLine("Az gép által gondolt szám a(z) {0}. Kissebb(>), nagyobb(<) vagy egyenlő(=) amire ön gondolt?", szam);
+
+            string valasz1 = Console.ReadLine();
+            if (valasz1 == "=") Console.WriteLine("Hohó, nagyon ügyes gép vagyok!");
+            else if (valasz1 == ">") szam1 = r.Next(1, szam);
+            else if (valasz1 == "<") szam1 = r.Next(szam, 1001);
+
+
+            Console.WriteLine("Akkor erre a számra gondoltam: {0}. Kissebb(<), nagyobb(>) vagy egyenlő(=) amire ön gondolt?", szam1);
+
+             string valasz2 = Console.ReadLine();
+            if (valasz2 == "=") Console.WriteLine("Hohó, nagyon ügyes gép vagyok!");
+            else if (valasz2 == ">") szam2 = r.Next(szam, szam1);
+            else if (valasz2 == "<") szam2 = r.Next(szam1, szam);
+            Console.WriteLine(szam2);
+
+            Console.WriteLine("Akkor erre a számra gondoltam: {0}. Kissebb(<), nagyobb(>) vagy egyenlő(=) amire ön gondolt?", szam2);
+
+            string valasz3 = Console.ReadLine();
+            if (valasz3 == "=") Console.WriteLine("Hohó, nagyon ügyes gép vagyok!");
+            else if (valasz3 == ">") szam3 = r.Next(szam1, szam2);
+            else if (valasz3 == "<") szam3 = r.Next(szam2, szam1);
+            Console.WriteLine(szam4);
+
+            Console.WriteLine("Akkor erre a számra gondoltam: {0}. Kissebb(<), nagyobb(>) vagy egyenlő(=) amire ön gondolt?", szam3);
+
+            string valasz4 = Console.ReadLine();
+            if (valasz4 == "=") Console.WriteLine("Hohó, nagyon ügyes gép vagyok!");
+            else if (valasz4 == ">") szam4 = r.Next(szam2, szam3);
+            else if (valasz4 == "<") szam4 = r.Next(szam3, szam2);
+            Console.WriteLine(szam4);
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/kitalalos_simple.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int also = 1;
+            int felso = 1000;
+            char valasz;
+
+            do
+            {
+                int tipp = (also + felso) / 2;
+                Console.WriteLine("A gép által gondolt szám {0}", tipp);
+                Console.WriteLine("Jól gondolta a gép? (< , > , =)");
+                valasz = Convert.ToChar(Console.ReadLine());
+                if (valasz == '=') Console.WriteLine("Ügyes gép vagyok!");
+                else if (valasz == '<') felso = tipp - 1;
+                else also = tipp + 1;
+                Console.WriteLine("alsó érték:{0}, felső érték:{1}", also, felso);
+            } while (valasz!= '=');
+
+            Console.ReadKey();
+
+        }
+    }
+}
